79cfbad7d005ad16ec1492954bcaaffe0a474b35
zdliuccit <335758663@qq.com>
2020-09-23 15:35:40 +0800

    feat: 啊啊啊

--- app/middleWares/static.js
+++ app/middleWares/static.js
@@ -7,5 +7,5 @@ import serverStatic from 'koa-static'
 const path = require('path')
 
 export default () => {
-  return serverStatic(path.join(process.cwd(), 'app/public'), { maxAge: 60 * 60 * 24 * 30 * 1000, gzip: true })
+  return serverStatic(path.join(process.cwd(), 'app/views'), { maxAge: 60 * 60 * 24 * 30 * 1000, gzip: true })
 }
